1b571ca8fd7aa199dc98226f029061efffb3ff26
ujun <zhaorujunfor@tianzehuitong.com>
2018-06-20 16:29:43 +0800

    build version

--- src/app/main/article.service.ts
+++ src/app/main/article.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
-import {Article,ArticleResponse,CommonResponse} from './types'
+import {Article, ArticleResponse, CommonResponse} from './types'
 import {Observable} from 'rxjs/Observable'
 import {joinQueryString} from '../utils'
 
